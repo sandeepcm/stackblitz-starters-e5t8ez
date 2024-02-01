f49f348437837511ac84687e5c7d83a43e004364
sandeepcm <sandeepkrdhiman05@gmail.com>
2024-02-01 18:06:00 +0530

    tailwind install

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import './style.css';
+import './index.css';
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1>
   );
 }
